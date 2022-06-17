1948c98a21152e5f2ae7d5092ada3b13412f61cb
Yeison <enmanuelquiterios@gmail.com>
2022-06-17 11:19:35 -0400

    Rewiewing steps

---
+++ index.js
@@ -0,0 +1,13 @@
+// Basic Reviews
+
+const nemo = ["nemo"];
+
+const findNemo = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "nemo") {
+      console.log("Found nemo");
+    }
+  }
+};
+
+findNemo(nemo);
