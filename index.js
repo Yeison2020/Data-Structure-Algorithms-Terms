5216cdfec2acde5fe4ef78b3749f7a8e073071ba
Yeison <enmanuelquiterios@gmail.com>
2022-06-17 11:23:19 -0400

    Adding Values and performance

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
 const nemo = ["nemo"];
 
 const findNemo = (arr) => {
+  // Measuring Performance
+
+  let t0 = performance.now();
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] == "nemo") {
       console.log("Found nemo");
     }
   }
+  let t1 = performance.now();
+  console.log(`Call to find nemo took ${t0 - t1} milliseconds`);
 };
 
 findNemo(nemo);
