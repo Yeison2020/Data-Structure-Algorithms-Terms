35c21c61c51d0c48b78f373c2908f87e9d8f61bb
Yeison <enmanuelquiterios@gmail.com>
2022-06-20 13:14:32 -0400

    Adding methods and understanding BigO

--- typeDatastructure.js
+++ typeDatastructure.js
@@ -1,3 +1,10 @@
 // Array Data Structures
 const arr = ["a", "b", "c", "d"];
 console.log(arr[3]);
+
+arr.push("Enmanuel");
+console.log(arr);
+
+arr.shift("Quiterios");
+arr.unshift("Casado");
+console.log(arr);
