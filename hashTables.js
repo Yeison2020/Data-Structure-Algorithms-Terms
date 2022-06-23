537f2754ad5c7df747ca47050db1b5c83e8be61e
Yeison <enmanuelquiterios@gmail.com>
2022-06-22 23:36:04 -0400

    Collitions in hash tables

---
+++ hashTables.js
@@ -0,0 +1,14 @@
+let user = {
+  age: 26,
+  name: "Yeison",
+  magic: true,
+  screa: function () {
+    console.log("HAHAHAHAH");
+  },
+};
+
+user.scream();
+user.spell = "Enmanuel";
+console.log(user);
+
+// Hash tables can cuase a lot collitions and slows down our ability to insert a retrive information
