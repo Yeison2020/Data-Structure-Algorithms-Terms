5ffdf9e0cb3bde0cb427691ee6f1d64173bfffb5
Yeison <enmanuelquiterios@gmail.com>
2022-06-22 23:44:25 -0400

    Creating Hash class

--- hashTables.js
+++ hashTables.js
@@ -12,3 +12,19 @@ user.spell = "Enmanuel";
 console.log(user);
 
 // Hash tables can cuase a lot collitions and slows down our ability to insert a retrive information
+
+/// HASH TABLES CLASS
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
