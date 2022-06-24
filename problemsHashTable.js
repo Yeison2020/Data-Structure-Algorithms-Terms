fa1266261a32a289884b0cf7ec7a54d7af5cac8f
Yeison <enmanuelquiterios@gmail.com>
2022-06-24 12:15:11 -0400

    Recurring Numbers

---
+++ problemsHashTable.js
@@ -0,0 +1,19 @@
+// Here Get the first recurrent Character
+// Meaning The number that gets repeated first in the Array
+
+// 1.
+const arr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+
+// Should return 2
+
+// 2.
+
+const arr1 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
+
+// Should return 1
+
+// 3.
+
+const arr2 = [2, 3, 4, 5];
+
+// should return undefined, because We do not have repeated Numbers
