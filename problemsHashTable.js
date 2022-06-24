878ac5dcb94bb6c80537aed79e96d0e2532349b3
Yeison <enmanuelquiterios@gmail.com>
2022-06-24 12:23:04 -0400

    Finding recurringNumbs

--- problemsHashTable.js
+++ problemsHashTable.js
@@ -4,6 +4,17 @@
 // 1.
 const arr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
 
+const findRepeatedNum = (arr) => {
+  if (arr.length < 2) {
+    return undefined;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      return arr[i];
+    }
+  }
+};
+
 // Should return 2
 
 // 2.
