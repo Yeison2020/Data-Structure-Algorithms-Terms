f2db3287b69e1b54c363344502650f770c1d3336
Yeison <enmanuelquiterios@gmail.com>
2022-06-24 12:41:46 -0400

    Completed version hash Table

--- problemsHashTable.js
+++ problemsHashTable.js
@@ -21,6 +21,18 @@ const findRepeatedNum = (arr) => {
 
 const arr1 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
 
+const firstRecurrentChar2 = (arr) => {
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      return arr[i];
+    } else {
+      map[arr[i]] = i;
+    }
+  }
+  return undefined;
+};
+
 // Should return 1
 
 // 3.
